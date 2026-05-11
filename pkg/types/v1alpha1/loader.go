59abb9af79f61fd346263f9840abd501bd01b8d7
Mateusz Urbanek <mateusz.urbanek@siderolabs.com>
2026-05-11 12:29:52 +0200

    feat(loader): add functional options for VEX data manipulation
    Enables post-load transformations of ExploitabilityData, starting with
    PURL overrides. Supports extensible Option pattern for future mutations
    without API changes.
    
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek@siderolabs.com>

--- pkg/types/v1alpha1/loader.go
+++ pkg/types/v1alpha1/loader.go
@@ -9,12 +9,27 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/openvex/go-vex/pkg/vex"
 	"go.yaml.in/yaml/v4"
 )
 
+// Option is a functional option manipulating ExploitabilityData.
+type Option func(*ExploitabilityData)
+
+// WithPURLOverride allows overriding the PURL in the ExploitabilityData.
+func WithPURLOverride(purl string) Option {
+	return func(data *ExploitabilityData) {
+		if data.IDs == nil {
+			data.IDs = make(map[vex.IdentifierType]string)
+		}
+
+		data.IDs[vex.PURL] = purl
+	}
+}
+
 // LoadExploitabilityData loads VEX data from an io.Reader.
 // The data should be in YAML format matching ExploitabilityData structure.
-func LoadExploitabilityData(reader io.Reader) (*ExploitabilityData, error) {
+func LoadExploitabilityData(reader io.Reader, opts ...Option) (*ExploitabilityData, error) {
 	data, err := io.ReadAll(reader)
 	if err != nil {
 		return nil, fmt.Errorf("error reading data: %w", err)
@@ -23,6 +38,11 @@ func LoadExploitabilityData(reader io.Reader) (*ExploitabilityData, error) {
 	var result ExploitabilityData
 	if err := yaml.Unmarshal(data, &result); err != nil {
 		return nil, fmt.Errorf("error unmarshalling data: %w", err)
+	}
+
+	// Apply options to the loaded data.
+	for _, opt := range opts {
+		opt(&result)
 	}
 
 	return &result, nil
